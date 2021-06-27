510059f829c1539c22cd128ec04e8a8bd486943f
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-06-27 16:12:31 -0400

    baseline build in index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+document.addEventListener("click", (event)=>{ console.log("You just clicked on == ", event.target) } )
+// "listen to TheDom"
+document.addEventListener("DOMContentLoaded", function(){ /* */ console.log("We, Are, Live")})
