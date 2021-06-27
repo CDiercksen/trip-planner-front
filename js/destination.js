a5ae1fd29eb062f29e83f028b51f44681e38d5a1
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-06-27 16:45:16 -0400

    baseline function added to destination.js

---
+++ js/destination.js
@@ -0,0 +1,10 @@
+console.log("This working?")
+
+function destinationMaker(name, image){
+    return{
+        name: name,
+        image: image
+    }
+}
+
+const chris = destinationMaker("Chris", "https://ca.slack-edge.com/T02MD9XTF-U01CA1HPEA1-g30a7a74eced-512")
